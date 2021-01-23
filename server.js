d1ab504014c6547499df9c53078fdacf7b52b384
Matthew Hayden <matthayden09@gmail.com>
2021-01-23 10:46:19 -0600

    setting up server and adding characters

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const characters = [
+    {
+        name: 'Yoda',
+        role: 'Jedi Master',
+        forcePoints: 10000,
+        age: 900,
+        avatar: '',
+        routeName: 'yoda'
+    },
+    {
+        name: 'Luke Skywalker',
+        role: 'Jedi Knight',
+        forcePoints: 5000,
+        age: 40,
+        avatar: '',
+        routeName: 'lukeskywalker'
+    },
+    {
+        name: 'Princess Leia',
+        role: 'General',
+        forcePoints: 1000,
+        age: 40,
+        avatar: '',
+        routeName: 'princessleia'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('May the Force be with you')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
