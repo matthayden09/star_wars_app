977f04ad82f692d02b9d5eeaf845ff29dc3d83f5
Matthew Hayden <matthayden09@gmail.com>
2021-01-23 13:00:34 -0600

    api done

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.url, res.statusCode)
+    next()
+})
 const characters = [
     {
         name: 'Yoda',
@@ -33,6 +39,10 @@ app.get('/', (req, res) => {
     res.send('May the Force be with you')
 })
 
+// HTML ROUTES
+
+// API ROUTES
+
 // /api/characters - show all character data
 app.get('/api/characters', (req, res) => {
     res.json(characters)
@@ -46,7 +56,18 @@ app.get('/api/characters/:routeName', (req, res) => {
     })
     res.json(character)
 })
+
+// add new characters
+app.post('/api/characters/add', (req, res) => {
+    // console.log(req.body)
+    const newCharacter = req.body
+    newCharacter.routeName = newCharacter.name.replace(/ /g, '').toLowerCase()
     
+    characters.push(req.body)
+    
+    res.status(200).send()
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 })
